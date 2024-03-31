5b9fded2c8989b4b09ecf6f478f28fadb86b99f1
Bharat Chawla <bharatchawla53@gmail.com>
2024-03-30 22:06:00 -0400

    Section 3

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import cors from "cors";
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import mongoose from 'mongoose';
+import UserRoutes from './Kanbas/users/routes.js';
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -15,5 +18,6 @@ Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000)
