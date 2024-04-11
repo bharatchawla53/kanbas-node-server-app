d085405482526610b259dbe4c2b24de10d4dad49
Bharat Chawla <bharatchawla53@gmail.com>
2024-04-11 15:58:50 -0400

    adds logging

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import EnrollmentRoutes from './Kanbas/enrollments/routes.js';
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
+console.log("FRONTEND_URL: ", process.env.FRONTEND_URL);
 app.use(cors({
     credentials: true,
     origin: process.env.FRONTEND_URL,
